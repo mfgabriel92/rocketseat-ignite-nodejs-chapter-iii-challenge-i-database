f46a65c1ddfefe4a8a33d5cf219984da28b27134
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2022-05-17 18:36:24 -0300

    Initial commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -13,18 +13,31 @@ export class UsersRepository implements IUsersRepository {
 
   async findUserWithGamesById({
     user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  }: IFindUserWithGamesDTO): Promise<User | undefined> {
+    const query = this.repository.findOne({
+      where: {
+        id: user_id
+      },
+      relations: ['games']
+    });
+
+    return query;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name;");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    first_name = first_name.toLowerCase();
+    last_name = last_name.toLowerCase();
+
+    return this.repository.query(`SELECT * 
+                                  FROM users 
+                                  WHERE LOWER(first_name) = $1 
+                                  AND LOWER(last_name) = $2`, [first_name, last_name] as any);
   }
 }
